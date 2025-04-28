ba433eb6757a0dd7ed01c78dbf91480579ad56c7
Gaurav Bhatt <gauravkapadibhatt@gmail.com>
2025-04-28 11:58:45 +0545

    Added some structure files and room list files.

--- jatte-starter/chat/views.py
+++ jatte-starter/chat/views.py
@@ -6,10 +6,8 @@ from django.contrib.auth.models import Group
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
-
 from account.forms import AddUserForm, EditUserForm
 from account.models import User
-
 from .models import Room
 
 
@@ -21,3 +19,12 @@ def create_room(request, uuid):
     Room.objects.create(uuid=uuid, client=name, url=url)
 
     return JsonResponse({'message': 'room created'})
+
+@login_required
+def admin(request):
+    rooms=Room.objects.all()
+    users=User.objects.filter(is_staff=True)
+    return render(request,'chat/admin.html',{
+        'rooms':rooms,
+        'users':users
+    })
